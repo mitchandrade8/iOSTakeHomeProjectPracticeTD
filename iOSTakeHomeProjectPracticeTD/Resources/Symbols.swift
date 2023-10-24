e22b03d0acf8828ef71a3b752bebf8fd9be66548
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-24 13:56:43 -0600

    --Updating: to current preparation of take home project

--- iOSTakeHomeProjectPracticeTD/Resources/Symbols.swift
+++ iOSTakeHomeProjectPracticeTD/Resources/Symbols.swift
@@ -5,4 +5,10 @@
 //  Created by Mitch Andrade on 10/24/23.
 //
 
-import Foundation
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
