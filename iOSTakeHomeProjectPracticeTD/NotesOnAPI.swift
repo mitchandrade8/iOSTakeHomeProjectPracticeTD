7306976a9d3ec96f14310078eaa7f5d0fe5468fc
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-24 15:35:10 -0600

    --Updating: to API Overview

--- iOSTakeHomeProjectPracticeTD/NotesOnAPI.swift
+++ iOSTakeHomeProjectPracticeTD/NotesOnAPI.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+// MARK: - REST API
+
+// REST: Representational State Transfer
+
+// API: Application Programming Interface
+/// Set of protocols to interact with a service 
+
+
+// An API is like a black box:
+/// You do not know the actual code is to make it work but you do that you can call certain definitions within to interact with it
+
+/// Think of it like a delivery service app, you do not know how uber eats works behind the scenes but they provide you with all the
+/// tools to easily order a big mac.
+
