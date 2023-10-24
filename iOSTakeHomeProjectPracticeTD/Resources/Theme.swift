241ab0e15740fb4062d02e68683389e1ada23bd7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-24 14:05:09 -0600

    --Updating: to current project setup

--- iOSTakeHomeProjectPracticeTD/Resources/Theme.swift
+++ iOSTakeHomeProjectPracticeTD/Resources/Theme.swift
@@ -5,4 +5,11 @@
 //  Created by Mitch Andrade on 10/24/23.
 //
 
-import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
