3d3ae6abee367e50b776df80045d6ba4bb65d8c9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-25 05:30:45 -0600

    --Updating: to using Postman to test the API

--- iOSTakeHomeProjectPracticeTD/Notes/NotesOnJSON.swift
+++ iOSTakeHomeProjectPracticeTD/Notes/NotesOnJSON.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+// MARK: - JSON
+/// Stands for ** Javascript Object Notation **
+
+// In industry this is one of the most common ways of sending and receiving data from API services
+
+/// Other alternatives
+
+/// A great way to also represent objects or store a collection of objects.
+
+/// Can use strings to decode dates from JSON formats 
